e00909dd7820e0f91d874f796ccf0eff42915c59
f2e-lzh <1938973212@qq.com>
2022-10-12 16:30:38 +0800

    第八次上传文件

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("js");
+console.log(`
+    初始化git git init
+    添加到待上传的暂存区 git add.
+    链接上远方的git仓库 git commit - m 第一次上传文件
+    Git远程添加origin git remote add origin
+    git推送 git push - u origin master
+
+    查看当前项目的修改状态 git status
+    查看以往所有人的提交记录 git log
+    查看某人以往的提交记录 git log--author = "李镇辉"
+
+    配置git用户名 git config--global user.name 'f2e-lzh'
+    配置github邮箱 git config--global user.email '928712883@qq.com'
+    检查配置的是否成功 git config--global--list
+
+    想要修改项目中的文件时如何操作 git add index.html
+
+
+    
+`);
